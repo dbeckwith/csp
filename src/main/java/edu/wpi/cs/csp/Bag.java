a227c15d7e9ee082b356a7af7f37ced19f85c4e3
Aditya Nivarthi <adityanivarthi19@gmail.com>
2016-02-16 15:50:14 -0500

    Updated currentSize management in Bag. Added comments and formatting.

--- src/main/java/edu/wpi/cs/csp/Bag.java
+++ src/main/java/edu/wpi/cs/csp/Bag.java
@@ -2,46 +2,85 @@ package edu.wpi.cs.csp;
 
 import java.util.HashSet;
 
+/**
+ * This class represents a bag that can hold items.
+ *
+ * @author Aditya Nivarthi
+ */
 public class Bag extends HashSet<Item> {
+
     protected final int MAX_SIZE;
     protected int currentSize = 0;
     protected double capacityPercentage = 0.9f;
 
-    public Bag(int maxSizw) {
-        this.MAX_SIZE = maxSizw;
+    /**
+     * Creates a new Bag instance with the specified max size.
+     *
+     * @param maxSize The maximum size for this bag.
+     */
+    public Bag(int maxSize) {
+        this.MAX_SIZE = maxSize;
     }
 
+    /**
+     * Adds the specified item to the bag.
+     *
+     * @param item The item to add to the bag.
+     * @return true if added, false otherwise
+     */
     @Override
     public boolean add(Item item) {
         if (super.add(item)) {
             item.setBag(this);
+            currentSize++;
             return true;
         }
 
         return false;
     }
 
+    /**
+     * Removes the specified object from the bag. If the object can be removed, it is. If it is of type Item, then its bag will be set to null.
+     *
+     * @param o The object to be removed.
+     * @return true if removes, false otherwise
+     */
     @Override
     public boolean remove(Object o) {
         if (super.remove(o)) {
             if (o instanceof Item) {
                 ((Item) o).setBag(null);
             }
+
+            currentSize--;
             return true;
         }
 
         return false;
     }
 
+    /**
+     * Moves the specified item from one bag to the other specified bag.
+     *
+     * @param item     The item to move to the other bag.
+     * @param otherBag The other bag to move the item into.
+     * @return true if removed from this bag and moved to the other bag, false otherwise
+     */
     public boolean move(Item item, Bag otherBag) {
         if (this.remove(item)) {
             item.setBag(otherBag);
+            currentSize--;
             return otherBag.add(item);
         }
 
         return false;
     }
 
+    /**
+     * Returns whether this bag is at the minimum required capacity.
+     *
+     * @return true if at minimum capacity, false otherwise
+     */
     public boolean isAtMinimumCapacity() {
         return Double.compare(currentSize, Math.floor(MAX_SIZE * capacityPercentage)) >= 0;
     }
--- src/main/java/edu/wpi/cs/csp/Item.java
+++ src/main/java/edu/wpi/cs/csp/Item.java
@@ -1,28 +1,89 @@
 package edu.wpi.cs.csp;
 
+/**
+ * This class represents an item that can be added into a {@link Bag}.
+ *
+ * @author Aditya Nivarthi
+ */
 public class Item {
-    protected Bag bag = null;
-    protected String name = "";
 
+    private Bag bag = null;
+    private String name = "";
+
+    /**
+     * Creates an Item instance with the specified container {@link Bag} and name.
+     *
+     * @param bag  The {@link Bag} containing this item.
+     * @param name The name of this item.
+     */
     public Item(Bag bag, String name) {
         this.bag = bag;
         this.name = name;
     }
 
+    /**
+     * Creates an Item instance with the specified name.
+     *
+     * @param name The name of this item.
+     */
     public Item(String name) {
         this.name = name;
         bag = null;
     }
 
+    /**
+     * Returns the {@link Bag} containing this item.
+     *
+     * @return a {@link Bag}
+     */
     public Bag getBag() {
         return bag;
     }
 
+    /**
+     * Sets the {@link Bag} that is containing this item.
+     *
+     * @param bag The {@link Bag} containing this item.
+     */
     public void setBag(Bag bag) {
         this.bag = bag;
     }
 
+    /**
+     * Returns the name of this item.
+     *
+     * @return a {@link String}
+     */
     public String getName() {
         return name;
+    }
+
+    /**
+     * Returns whether the specified object is "equal" to this object.
+     *
+     * @param o The object to compare to this object.
+     * @return true if equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Item item = (Item) o;
+
+        if (bag != null ? !bag.equals(item.bag) : item.bag != null) return false;
+        return name.equals(item.name);
+    }
+
+    /**
+     * Returns the hashcode for this class.
+     *
+     * @return an integer
+     */
+    @Override
+    public int hashCode() {
+        int result = bag != null ? bag.hashCode() : 0;
+        result = 31 * result + name.hashCode();
+        return result;
     }
 }
